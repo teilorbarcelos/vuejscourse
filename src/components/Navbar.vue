4acd6dadc9291765fdd1474d7218aa43a64f4d5a
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-05-21 18:50:52 -0300

    project reinitialized

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <router-link>
+      <img src="" alt="" id="logo">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Navbar"
+}
+</script>
+
+<styled scoped>
+
+</styled>
